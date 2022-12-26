f787668c58b78d43e89da10bdec224142f4d4770
Kavyadeep Dev <devkavyadeep@gmail.com>
2022-12-26 23:59:49 +0530

    add: dwm config

---
+++ dwm/themes/onedark.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#282c34";
+static const char col_gray2[]       = "#282c34";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#61afef";
+
