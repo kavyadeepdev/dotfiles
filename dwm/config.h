f220b0d16e53b3d8cb4fe306169f5c9dbc7ea2f2
Kavyadeep Dev <devkavyadeep@gmail.com>
2023-02-12 14:15:46 +0530

    fix: update dwm config

--- dwm/config.h
+++ dwm/config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -71,7 +71,7 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:","-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 #include "shiftview.c"
--- dwm/themes/ayu-dark.h
+++ dwm/themes/ayu-dark.h
@@ -1,5 +1,5 @@
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#282a36";
+static const char col_gray1[]       = "#0a0e14";
+static const char col_gray2[]       = "#0a0e14";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#53bdfa";
