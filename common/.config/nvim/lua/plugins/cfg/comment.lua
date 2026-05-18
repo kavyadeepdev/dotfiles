04873b1efe4d0800ab82c32b7c292a070b0ba2a7
Kavyadeep Dev <kavyadeepdev@proton.me>
2026-05-18 17:42:51 +0530

    feat(nvim): add lazy git and python comment support

--- common/.config/nvim/lua/plugins/cfg/comment.lua
+++ common/.config/nvim/lua/plugins/cfg/comment.lua
@@ -2,4 +2,12 @@ return {
 	"numToStr/Comment.nvim",
 	opts = {},
 	lazy = false,
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "python",
+			callback = function()
+				vim.bo.commentstring = "# %s"
+			end,
+		})
+	end,
 }
